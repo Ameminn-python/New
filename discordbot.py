cb2ec162d18ef6dc28838b2d9a812de57a60f8a9
あめみん <63637891+Ameminn-python@users.noreply.github.com>
2020-06-14 09:55:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import discord
 import os
 import traceback
-from manege import 
+from manege import pm
 
 bot = commands.Bot(command_prefix='/',help_command=None)
 token = os.environ['DISCORD_BOT_TOKEN']
